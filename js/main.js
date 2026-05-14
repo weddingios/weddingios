6934f205d6a8b1cc457d5d3ef817a86303401c1e
Naturalhoneyleb <121385304+Naturalhoneyleb@users.noreply.github.com>
2026-05-14 17:36:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:false,
+	    autoplay: false,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
+
+
+
+
+// Firework object 
+var $Firework1 = $('.firework1');
+var $Firework2 = $('.firework2');
+var $Firework3 = $('.firework3');
+var $Firework4 = $('.firework4');
+var $Firework5 = $('.firework5');
+
+const particleCount = 60;
+
+// Color generate
+const randomValue = (max, min) => Math.floor(Math.random() * (max - min) + min);
+const randomColor = () => 
+  `rgba(${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(100, 50)}%)`;
+
+// Genterate particle
+const randomPosition = (max, min) => ({
+  x: `${randomValue(max, min)}%`,
+  y: `${randomValue(max, min)}%`
+});
+const createParticle = color => `radial-gradient(circle, ${color} 0.2vmin, #0000 0)`;
+
+// Genterate particle style
+const particleStyle = (particles, positions) => ({
+  backgroundImage: particles.join(','),
+  backgroundPositionX: positions.x.join(',').toString(),
+  backgroundPositionY: positions.y.join(',').toString()
+});
+
+// Generate firework
+const createFirework = (firework, n) => {
+  var particles = [];
+  var positions = {
+    x: [],
+    y: []
+  };
+  
+  const color = randomColor();
+  for(let i = 0; i < n; i++) {
+    const {x, y} = randomPosition(100, 0);
+    particles.push(createParticle(color));
+    positions.x.push(x);
+    positions.y.push(y);
+  }
+  
+  firework.css({
+    ...particleStyle(particles, positions),
+    top: `${randomValue(50, 20)}%`,
+    left: `${randomValue(80, 30)}%`
+  });
+};
+
+// Animate settings
+const duration = second => second * 1000;
+const effect = () => ({
+  opacity: 1,
+  width: '30vmin'
+});
+const reset = () => ({
+  width: ''
+});
+
+const animate = (firework, time) => {
+  createFirework(firework, particleCount);
+  firework.delay(time).animate(effect(), duration(1.2), 'swing', () => {
+    firework.css(reset());
+    animate(firework, time);
+  })
+};
+
+animate($Firework1, duration(0));
+animate($Firework2, duration(1));
+animate($Firework3, duration(2));
+animate($Firework4, duration(3));
+animate($Firework5, duration(4));
+
+
+
